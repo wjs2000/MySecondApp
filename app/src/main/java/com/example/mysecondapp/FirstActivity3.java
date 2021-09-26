a7152a744117a56f7e9a584f1156b5f25e987db4
YouPeng <1256798467@qq.com>
2021-09-26 19:23:06 +0800

    第三版.

--- app/src/main/java/com/example/mysecondapp/FirstActivity3.java
+++ app/src/main/java/com/example/mysecondapp/FirstActivity3.java
@@ -3,13 +3,14 @@ package com.example.mysecondapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import java.math.BigDecimal;
 
 public class FirstActivity3 extends AppCompatActivity {
-
+    private static final String TAG = "FirstActivity3";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,12 +19,19 @@ public class FirstActivity3 extends AppCompatActivity {
 
 
     public void click4(View btn){
+       // Log.i(TAG, "click4: ");
         TextView subjectView = findViewById(R.id.first_subject);
         TextView showView = findViewById(R.id.first_show);
-        double rmb = Double.parseDouble(subjectView.getText().toString());
-        if(btn.getId() == R.id.first_btn_dollar){showView.setText(transfor(rmb*0.1548)+" 美元");}
-        else if(btn.getId() == R.id.first_btn_euro){showView.setText(transfor(rmb*0.1321)+" 欧元");}
-        else if(btn.getId() == R.id.first_btn_won){showView.setText(transfor(rmb*181.46)+" 韩元");}
+        if(subjectView.getText().toString().length()>0){
+            double rmb = Double.parseDouble(subjectView.getText().toString());
+            if(btn.getId() == R.id.first_btn_dollar){showView.setText(transfor(rmb*0.1548)+" 美元");}
+            else if(btn.getId() == R.id.first_btn_euro){showView.setText(transfor(rmb*0.1321)+" 欧元");}
+            else if(btn.getId() == R.id.first_btn_won){showView.setText(transfor(rmb*181.46)+" 韩元");}
+        }
+        else{
+            showView.setText(R.string.RMBshow);
+        }
+
     }
 
     private double transfor(double a){//保留两位小数
